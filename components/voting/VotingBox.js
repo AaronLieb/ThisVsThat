164d64b59f5580bf7389bf8491650c1b3bcc46f9
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-04-06 13:18:47 -0700

    added VotingBox component

---
+++ components/voting/VotingBox.js
@@ -0,0 +1,13 @@
+import { createPortal } from 'react-dom';
+import classes from './VotingBox.module.css';
+
+export default function VotingBox(props){
+
+    const color = props.color;
+
+    return(
+        <div className={color == 'red' ? classes.container_red : classes.container_blue}>
+            
+        </div>
+    )
+}

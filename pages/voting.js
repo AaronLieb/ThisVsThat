c3ca85ebdaf6c5b308a6e8cbbf2de2536aecea0a
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-04-06 13:21:01 -0700

    created voting page

---
+++ pages/voting.js
@@ -0,0 +1,11 @@
+import VotingBox from "../components/voting/VotingBox";
+import VotingLayout from "../components/voting/VotingLayout";
+
+
+export default function Voting(){
+    return (
+        <div>
+            <VotingLayout/>
+        </div>
+    )
+}

d060566c0d99501cc470580cb61cc02e4abb93e8
AaronLieb <aarnliebrmn@gmail.com>
2022-04-13 12:50:45 -0700

    switched back to modular scss instead of styled jsx

--- components/index/Title.js
+++ components/index/Title.js
@@ -1,51 +1,13 @@
-import { red, blue, gray } from "/styles/colors.js";
+import styles from "/styles/title.module.scss";
 
 export default function Title() {
   return (
     <div>
-      <h1>
-        <span className="red">This</span>
-        <span className="gray"> vs </span>
-        <span className="blue">That</span>
-        <h2>{red}</h2>
+      <h1 className={styles.title}>
+        <span className={styles.red}>This</span>
+        <span className={styles.gray}> vs </span>
+        <span className={styles.blue}>That</span>
       </h1>
-
-      <style jsx>{`
-        @mixin floatyColor($baseColor) {
-          color: $baseColor;
-          $dark: desaturate(darken($baseColor, 30%), 30%);
-          text-shadow: 0px 1px $dark, 0px 2px $dark, 0px 3px $dark,
-            0px 4px $dark, 0px 5px $dark, 0px 6px $dark, 0px 20px 5px #ddd;
-        }
-
-        h1 {
-          font-size: 12vw;
-          margin: 50px;
-          user-select: none;
-          animation: float 5s ease-in-out infinite;
-          .red {
-            @include floatyColor(#ff7b7b);
-          }
-          .gray {
-            @include floatyColor(#e8e8e8);
-          }
-          .blue {
-            @include floatyColor(#78aeff);
-          }
-        }
-
-        @keyframes float {
-          0% {
-            transform: translate(0, 0);
-          }
-          50% {
-            transform: translate(0, 10px);
-          }
-          100% {
-            transform: translate(0, 0);
-          }
-        }
-      `}</style>
     </div>
   );
 }
--- components/shared/Button.js
+++ components/shared/Button.js
@@ -1,47 +1,5 @@
-import { red, blue, gray } from "/styles/colors.js";
+import styles from "/styles/button.module.scss";
 
 export default function Button({ size = 30, color, children }) {
-  return (
-    <a className={color}>
-      {children}
-
-      {/* <style jsx>{`
-        @mixin buttonHoverAndClick($baseColor) {
-          background-color: $baseColor;
-          box-shadow: 0px 6px 1px desaturate(darken($baseColor, 30%), 30%);
-          &:hover {
-            background-color: desaturate(darken($baseColor, 10%), 30%);
-          }
-          &:active {
-            transform: translate(0px, 6px);
-            box-shadow: 0px 1px 1px desaturate(darken($baseColor, 30%), 30%);
-            transition: transform 0.08s, box-shadow 0.08s;
-          }
-        }
-
-        a {
-          cursor: pointer;
-          padding: ${size}px;
-          font-size: ${size}px;
-          color: black !important;
-          border-radius: ${size * 0.666}px;
-          margin: 10px;
-          text-align: center;
-          justify-content: center;
-          user-select: none;
-          @include buttonHoverAndClick(${gray});
-        }
-
-        .red {
-          color: white !important;
-          @include buttonHoverAndClick(${red});
-        }
-
-        .blue {
-          color: white !important;
-          @include buttonHoverAndClick(${blue});
-        }
-      `}</style> */}
-    </a>
-  );
+  return <a className={`${styles.button} ${styles[color]}`}>{children}</a>;
 }

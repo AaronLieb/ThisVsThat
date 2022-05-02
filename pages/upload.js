d00d2ed754d47bfd807a4b85bc5acdba4c713945
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-05-02 13:49:22 -0700

    Removed upload subfolder in components folder
    Decided to re use components

--- pages/upload.js
+++ pages/upload.js
@@ -1,11 +1,17 @@
 import Header from "../components/shared/Header";
-import UploadLayout from "../components/upload/UploadLayout";
+import Title from "../components/shared/Title";
+import Button from "../components/shared/Button";
+import styles from "../styles/single_button_page.module.scss";
 
 export default function upload() {
   return (
-    <div>
+    <div className={styles.content}>
       <Header />
-      <UploadLayout />
+      <Title className={styles.title} text="Upload your list to vote"/>
+      <Button className={styles.button} color="blue">
+      <img src='/downloadpicture.png' alt="downloadpicture"/>
+        Upload.csv
+        </Button>
     </div>
   );
 }

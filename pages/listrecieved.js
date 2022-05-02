97e3e8169a52f5cdaa63932218c8c0b4f74eadb3
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-05-02 13:44:13 -0700

    Removed list received subfolder in components folder
    Decided to re use components rather than make specific ones

--- pages/listrecieved.js
+++ pages/listrecieved.js
@@ -1,11 +1,15 @@
 import Header from "../components/shared/Header";
-import ListRecievedLayout from "../components/listrecieved/ListRecievedLayout";
+import FinishedLayout from "../components/finishedranking/FinishedLayout";
+import Title from "../components/shared/Title";
+import Button from "../components/shared/Button";
+import styles from "../styles/single_button_page.module.scss";
 
 export default function listrecieved() {
   return (
-    <div>
+    <div className={styles.content}>
       <Header />
-      <ListRecievedLayout />
+      <Title className={styles.title} text="Sucess! We recieved your list."/>
+      <Button className={styles.button} color="blue">Vote Now</Button>
     </div>
   );
 }

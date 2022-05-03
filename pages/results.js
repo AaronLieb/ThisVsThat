d15b79894d9762a02b266ee38824d6ff6231f5fd
jorgeemejia <jorgeemejia@csu.fullerton.edu>
2022-05-02 20:50:03 -0700

    Results page renders a result component for each ranking

--- pages/results.js
+++ pages/results.js
@@ -1,11 +1,24 @@
 import ResultsLayout from "../components/results/ResultsLayout";
-
+import Header from "../components/shared/Header";
+import styles from "../styles/results.module.scss";
+import Result from "../components/results/Result"
 
 
 export default function Results() {
+
+    let items=['item 1', 'item 2', 'item 3'];
+    let itemList=[];
+    items.forEach((item,index)=>{
+        itemList.push(<Result text={item}/>)
+    })
+
     return(
         <div>
-            <ResultsLayout/>
+            <Header/>
+            <h1 className={styles.h1}>Your rankings:</h1>
+            <div className={styles.rankings_container}>
+                {itemList}
+            </div>
         </div>
     )
 }

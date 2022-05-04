930dadaf8b12214bda67ecd986877028bf07671a
AaronLieb <aarnliebrmn@gmail.com>
2022-05-04 12:12:57 -0700

    fixed css import ubug

--- components/shared/Panel.js
+++ components/shared/Panel.js
@@ -1,4 +1,4 @@
-import styles from "./Panel.module.css";
+import styles from "./Panel.module.scss";
 
 export default function Panel({ children }) {
   return <div className={styles.container}>{children}</div>;
